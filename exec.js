f0510b2cb3c21cebd954a20c467a47490105d189
Bradley Cooper <bradley.david.cooper@gmail.com>
2018-06-28 14:11:03 -0600

    adding initial code

---
+++ exec.js
@@ -0,0 +1,22 @@
+const cp = require('child_process');
+
+const { logger } = require('./simpleLogger');
+
+exports.asyncExec = (command, options = { log: false, cwd: process.cwd() }) => {
+  if (options.log) {
+    logger.debug(command);
+  }
+
+  return new Promise((done, failed) => {
+    cp.exec(command, { ...options }, (err, stdout, stderr) => {
+      if (err) {
+        const newError = new Error(err);
+        newError.stdout = stdout;
+        newError.stderr = stderr;
+        failed(newError);
+        return;
+      }
+      done({ stdout, stderr });
+    });
+  });
+};
